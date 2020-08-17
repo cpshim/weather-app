e2c42b43a2a593be3be32990fc0d6bc404bf745e
Chris <chris.shim1999@hotmail.com>
2020-08-17 16:53:49 -0400

    Implemented React frontend to read JSON payload of the Express backend. Allows users to search for a city and shows the current weather and daily weather for the next week.

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var express = require('express');
+var path = require('path');
 
-var homeRouter = require('./routes/home');
 var searchRouter = require('./routes/search');
 
 var app = express();
 
-app.use('/', homeRouter);
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.use('/search', searchRouter);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + 'client/build/index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
--- routes/search.js
+++ routes/search.js
@@ -5,14 +5,22 @@ var fetch = require('node-fetch');
 
 require('dotenv').config()
 
-router.get('/:city', cors(), async(req, res, next) => {
+router.get('/:city', cors(), async (req, res, next) => {
     const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&appid=${process.env.API_KEY}`,
         { mode: "cors" }
     );
     const weatherData = await response.json();
-    console.log(weatherData);
     res.json(weatherData);
+});
+
+router.get('/detailed/:lat&:lon', cors(), async (req, res, next) => {
+    const detailedData = await fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${req.params.lat}&lon=${req.params.lon}&exclude=minutely&appid=${process.env.API_KEY}`,
+        { mode: "cors" }
+    );
+    const detailedWeatherData = await detailedData.json();
+    res.json(detailedWeatherData);
 });
 
 module.exports = router;
